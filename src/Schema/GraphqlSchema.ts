02a0f94b330c4d99b6f9954a7e2aa66b99185fcd
yousouf9 <ibraheemyousouf@gmail.com>
2022-12-20 22:59:43 +0100

    added token

--- src/Schema/GraphqlSchema.ts
+++ src/Schema/GraphqlSchema.ts
@@ -1,13 +1,27 @@
 import { gql } from "apollo-server-express";
 const typeDefs = gql`
-  type Tokens {
-    accessToken: String
-    refreshToken: String
+  type User {
+    username: String
+    email: String
+    password: String
+    id: String
+    token: String
   }
+  input RegisterInput {
+    username: String
+    email: String
+    password: String
+  }
+
+  input LoginInput {
+    email: String
+    password: String
+  }
+
   type Mutation {
     login(username: String, password: String!): Tokens
+    registerUser(registerInput: RegisterInput): User
   }
 `;
 
-//export schema and same and what
 export {typeDefs}
